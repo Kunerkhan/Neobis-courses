8d8993adac13982c7fef8aab711d3c431af98037
Kunerkhan <kakerov@gmail.com>
2019-02-01 13:20:22 +0600

    Added build for deploying

---
+++ build/precache-manifest.a1dac8dc967ed4b46f7d5878035a3c56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "582c40ca7201a8ddebc1ee1adf8da070",
+    "url": "/static/media/coding.582c40ca.jpg"
+  },
+  {
+    "revision": "9442b3c25c49f16f48cb00a9b3d26727",
+    "url": "/static/media/buffer.9442b3c2.svg"
+  },
+  {
+    "revision": "c147ba98bd4183ec14ab4ae66191b51d",
+    "url": "/static/media/android_phone.c147ba98.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f94694e927414afc52b7",
+    "url": "/static/js/main.f94694e9.chunk.js"
+  },
+  {
+    "revision": "8d9183621398347f15c1",
+    "url": "/static/js/1.8d918362.chunk.js"
+  },
+  {
+    "revision": "f94694e927414afc52b7",
+    "url": "/static/css/main.ec260756.chunk.css"
+  },
+  {
+    "revision": "57a432317aa8798fa841810664786b08",
+    "url": "/index.html"
+  }
+];
